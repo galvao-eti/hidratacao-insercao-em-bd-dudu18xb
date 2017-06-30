7550163f3771db9c76adc31a91d8d3f421dabfac
dudu18xb <eduardorocha460@gmail.com>
2017-06-29 22:29:56 -0300

    criando uma insercao do produto dentro do repositorio

---
+++ src/repositorio/Produto.php
@@ -0,0 +1,20 @@
+<?php
+
+class Produto implements Repositorio {
+
+    private $conn;
+
+    public function save() {
+        $st = $this->conn->prepare("insert into produto (nome ,valor) values (:nome,:valor)");
+
+        $st->bindParam(':nome', $this->controller->nome);
+        $st->bindParam(':valor', $this->controller->valor);
+
+        $st->execute();
+
+        if ($st->errorCode() <> '00000') {
+            throw new \Exception($st->errorInfo()[2]);
+        }
+    }
+
+}
