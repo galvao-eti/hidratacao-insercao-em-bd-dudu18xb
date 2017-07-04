2a8ee54abd723d77832ac3adf55d5f20cf8138bf
dudu18xb <eduardorocha460@gmail.com>
2017-07-04 20:58:46 -0300

    adicionando a classe e os metodos de usuarios

---
+++ job/User.php
@@ -0,0 +1,43 @@
+<?php
+namespace job;
+
+use job\Connection;
+
+class User {
+
+    private $email;
+    private $senha;
+
+    public function getEmail () 
+    {
+        return $this->email;
+    }
+
+    public function setEmail ( $email ) 
+    {
+        $this->email = $email;
+    }
+
+    public function getSenha () 
+    {
+        return $this->senha;
+    }
+
+    public function setSenha ( $senha ) 
+    {
+        $this->senha = $senha;
+    }
+
+    public function salvar ( $conexao ) 
+    {
+        $sql = "insert into usuario (email, senha) values(:email, :senha)";
+        $stmt = $conexao->prepare($sql);
+        $resultado = $stmt->execute(array(
+            ':email' => $this->email,
+            ':senha' => $this->senha
+        ));
+        return $resultado;
+    }
+
+    use Traits\Trait;
+}
