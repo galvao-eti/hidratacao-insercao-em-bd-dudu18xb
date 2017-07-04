c2a3e5782861924b8c5461c7aa3e7530e42327ea
dudu18xb <eduardorocha460@gmail.com>
2017-07-04 20:57:26 -0300

    adicionando a classe e os metodos do produto

---
+++ job/Product.php
@@ -0,0 +1,41 @@
+<?php
+namespace job;
+
+class Product {
+
+    private $nome;
+    private $valor;
+
+    public function getNome () 
+    {
+        return $this->nome;
+    }
+
+    public function setNome ( $nome ) 
+    {
+        $this->nome = $nome;
+    }
+    
+    public function getValor () 
+    {
+        return $this->valor;
+    }
+
+    public function setValor ( $valor ) 
+    {
+        $this->valor = $valor;
+    }
+
+    public function salvar ( $conexao ) 
+    {
+        $sql = "insert into produto (nome, valor) values(:nome, :valor)";
+        $stmt = $conexao->prepare($sql);
+        $resultado = $stmt->execute(array(
+            ':nome' => $this->nome,
+            ':valor' => $this->valor
+        ));
+        return $resultado;
+    }
+
+    use Traits\Trait;
+}
